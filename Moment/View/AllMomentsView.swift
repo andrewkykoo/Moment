4a887a25fa1a3b289ee3a40dec760e3993470b53
Andrew Koo <andrewkykoo@gmail.com>
2024-01-19 16:38:52 -0500

    update moments list to display additional information

--- Moment/View/AllMomentsView.swift
+++ Moment/View/AllMomentsView.swift
@@ -10,9 +10,31 @@ import SwiftUI
 struct AllMomentsView: View {
     @ObservedObject var viewModel: MomentsViewModel
     
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
+    
     var body: some View {
         List(viewModel.moments) { moment in
-            Text(moment.content)
+            VStack(alignment: .leading) {
+                Text(moment.content)
+                    .lineLimit(1)
+                Text("\(moment.dateCreated, formatter: dateFormatter)")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                
+                HStack {
+                    Text(moment.momentType.rawValue)
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                }
+            }
+            .onTapGesture {
+                // navigate to SingleMomentView
+            }
         }
         .onAppear {
             viewModel.fetchMoments()
